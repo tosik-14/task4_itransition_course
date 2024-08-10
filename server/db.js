270ab218822f56374fe69eed32f21078ddeadfc5
tosik-14 <anton.mikhailouski@gmail.com>
2024-08-10 15:51:21 +0300

    update server to use render

--- server/db.js
+++ server/db.js
@@ -1,4 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+module.exports = pool.promise();
+
+/*const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
@@ -7,4 +19,4 @@ const pool = mysql.createPool({
   database: 'task4_users'
 });
 
-module.exports = pool.promise();
+module.exports = pool.promise();*/
--- server/index.js
+++ server/index.js
@@ -3,6 +3,26 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const db = require('./db');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+/*const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const db = require('./db');
 
 const app = express();
 const port = 5000;
@@ -16,6 +36,6 @@ app.use('/api/users', userRoutes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
+*/
 
 //cd itransition_course\task4
