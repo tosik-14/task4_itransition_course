52b6edf8fe5102cd1909bef277d64168b25abc89
tosik-14 <anton.mikhailouski@gmail.com>
2024-08-10 20:51:25 +0300

    Add cloud db

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const db = require('./db');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3306;
 
 app.use(cors());
 app.use(express.json());
