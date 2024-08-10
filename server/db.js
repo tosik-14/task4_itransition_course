3ad3c62ab8c7564786cff1ea63e4044cd362e99f
tosik-14 <anton.mikhailouski@gmail.com>
2024-08-10 20:50:06 +0300

    Add cloud db

--- server/db.js
+++ server/db.js
@@ -8,6 +8,11 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 module.exports = pool.promise();
 
 /*const mysql = require('mysql2');
