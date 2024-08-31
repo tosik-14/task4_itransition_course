535ce33bf5aae1a5cfc754a45a8fdfb9543a4ee4
tosik-14 <anton.mikhailouski@gmail.com>
2024-08-31 12:42:05 +0300

    task4

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,19 +1,8 @@
-/*import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token');
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;*/
-
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token'); // Предполагаем, что токен хранится в localStorage
+  const isAuthenticated = !!localStorage.getItem('token'); 
 
   return isAuthenticated ? children : <Navigate to="/" />;
 };
